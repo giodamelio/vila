fc02010012315ba76767374ec3926daf0268cac3
Gio d'Amelio <giodamelio@gmail.com>
2019-08-22 05:46:40 +0000

    Parse simple yaml OpenAPI spec

---
+++ src/parse.js
@@ -0,0 +1,10 @@
+const fs = require('fs').promises;
+
+const yaml = require('js-yaml');
+
+module.exports = async function parse(openAPISpecPath) {
+  const specRaw = await fs.readFile(openAPISpecPath);
+  const specParsed = yaml.safeLoad(specRaw);
+
+  return specParsed;
+}
---
+++ test/parse.spec.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const parse = require('../src/parse');
+
+describe('Parse OpenAPI specs', () => {
+  it('Parse Yaml spec', async () => {
+    expect.assertions(1);
+    const spec = await parse(path.join(__dirname, 'openapi_specs/simple.yaml'));
+
+    expect(spec).toBeDefined();
+  });
+
+  it('Spec file does not exist', async () => {
+    expect.assertions(1);
+
+    await expect(
+      parse('this file does not exist')
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"ENOENT: no such file or directory, open 'this file does not exist'"`
+    );
+  });
+});
